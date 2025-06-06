0e4e9d1d810fb1a47a194509a448cbcf4218b845
kylianadit <adityaprakasa757@gmail.com>
2025-06-06 10:22:53 +0700

    nambahin waktu carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -747,7 +747,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Start auto-scroll
     function startAutoScroll() {
         if (autoScrollInterval) clearInterval(autoScrollInterval);
-        autoScrollInterval = setInterval(nextSlide, 4000); // 4 detik
+        autoScrollInterval = setInterval(nextSlide, 10000); // 10 detik
         console.log('Auto-scroll started');
     }
     
